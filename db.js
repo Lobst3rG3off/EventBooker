775d7aa9ce7cec0cd1b29c01bb3ef011fd3bd7c8
Lobst3rG3off <erip.pasei@gmail.com>
2020-03-05 18:18:48 -0500

    finished database control.

--- db.js
+++ db.js
@@ -10,3 +10,58 @@ db.collection('events').onSnapshot(snapshot => {
      showEvents(event.doc.data(), event.doc.id)
  });
 })
+//adds new event
+const addNewEvent = () => {
+    const event = {
+        //grabs data from form fields
+      name: form.name.value,
+      attendee: form.attendee.value,
+      booked: 0,
+      description: form.description.value,
+      status: parseInt(form.status.value, 10)
+    }
+      db.collection('events').add(event)
+      .then(() => {
+      // Reset the form values
+      form.name.value = "",
+      form.attendee.value = "",
+      form.description.value = "",
+      form.status.value = ""
+  
+      alert('Your event has been successfully saved')
+      })
+      .catch(err => console.log(err))
+  }
+//create empty array for events to go into
+  let bookedEvents = [];
+
+  //checks to see if event already exists.
+  const bookEvent = (booked, id) => {
+    const getBookedEvents = localStorage.getItem('booked-events');
+  
+    //if it shares the same id do not book.
+      if (getBookedEvents) {
+       bookedEvents = JSON.parse(localStorage.getItem('booked-events'));
+        if(bookedEvents.includes(id)) {
+          alert('Seems like you have already booked this event') 
+        } 
+        else {
+          saveBooking(booked, id)
+       }
+      } 
+      else {
+          saveBooking(booked, id)
+      }
+  };
+  //adds to the booked counter for event.
+  const saveBooking = (booked, id) => {
+      bookedEvents.push(id);
+      localStorage.setItem('booked-events', JSON.stringify(bookedEvents));
+  
+      const data = { booked: booked +1 }
+      db.collection('events').doc(id).update(data)
+      .then(() => alert('Event successfully booked'))
+      .catch(err => console.log(err))
+  }
+
+  
